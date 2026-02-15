a6c473a5c08a766954fd760a599558bb8c332aa8
suzanaai <angelska327@gmail.com>
2026-02-15 11:05:44 -0600

    vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
+// import { BrowserRouter } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
 import { UserProvider } from './contexts/UserContext.jsx';
 
